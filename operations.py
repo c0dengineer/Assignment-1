af49e704901a35985e54d5bb71fb49ef43d0ced7
c0dengineer <navyaroshni06@gmail.com>
2025-07-05 17:49:51 +0530

    Create operations.py

---
+++ operations.py
@@ -0,0 +1,17 @@
+# Task-1
+
+a = input('Enter the first number: ')
+a = int(a)
+b = input('Enter the second number: ')
+b = int(b)
+
+add = a+b
+subtract = a-b
+product = a*b
+divide = a/b
+
+print('\n')
+print('Additon: ', add)
+print('Subtarction: ', subtract)
+print('Multiplication: ',product)
+print('Division: ',divide)
